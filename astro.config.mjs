5b534935a82c49ea7bf670cd13c6e635ea033e01
shorif2 <shorifahamed571@gmail.com>
2025-07-06 22:44:56 +0600

    vercel error fixing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-// import vercel from "@astrojs/vercel";
 import vercel from "@astrojs/vercel/serverless";
 
 import tailwindcss from "@tailwindcss/vite";
