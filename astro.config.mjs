e6d6a75153a8a1aacfd72a2fb5b5eec334f53d37
shorif2 <shorifahamed571@gmail.com>
2025-07-06 22:17:11 +0600

    fix deployment issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
-import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel/server";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: "server",
-  // adapter: vercel(),
-  adapter: node({ mode: "standalone" }),
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
